faf8719a970ba65a9cb3e3683f2cda9d1d38a167
Thomas Byr <tbouyer2000@gmail.com>
2022-03-03 20:16:55 +0100

    tests

---
+++ tests/tests.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "macros.h"
+
+unsigned long _no_asserts = 0;
+
+void dummy_test() {
+    assert_eq(1, 1);
+    assert_neq(1, 2);
+    assert_lt(1, 2);
+    assert_gt(2, 1);
+    assert_leq(1, 1);
+    assert_geq(2, 2);
+}
+
+int main() {
+    test_case(dummy_test);
+    return 0;
+}
