d64e9f9b707c2960c3f9b5688b9f5610f52f02bc
Thomas Byr <tbouyer2000@gmail.com>
2022-03-05 19:46:30 +0100

    efficient pieces for cloning

---
+++ includes/piece.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Piece {
+  public:
+    static const int None = 0;
+    static const int King = 1;
+    static const int Pawn = 2;
+    static const int Knight = 3;
+    static const int Bishop = 4;
+    static const int Rook = 5;
+    static const int Queen = 6;
+
+    static const int White = 8;
+    static const int Black = 16;
+};
