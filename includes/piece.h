497d0ebd57a2b3a6ce45dec818502b4f7d7db545
Thomas Byr <tbouyer2000@gmail.com>
2022-03-06 15:05:41 +0100

    pieces

--- includes/piece.h
+++ includes/piece.h
@@ -1,15 +1,24 @@
 #pragma once
 
+#include <unordered_set>
+
 class Piece {
   public:
-    static const int None = 0;
-    static const int King = 1;
-    static const int Pawn = 2;
-    static const int Knight = 3;
-    static const int Bishop = 4;
-    static const int Rook = 5;
-    static const int Queen = 6;
+    inline static const int None = 0;
+    inline static const int King = 1;
+    inline static const int Pawn = 2;
+    inline static const int Knight = 3;
+    inline static const int Bishop = 4;
+    inline static const int Rook = 5;
+    inline static const int Queen = 6;
 
-    static const int White = 8;
-    static const int Black = 16;
+    inline static const int White = 8;
+    inline static const int Black = 16;
+
+    inline static const int type_mask = 7;
+    inline static const int color_mask = 24;
+
+  protected:
+    int pos;
+    std::unordered_set<int> controlled_squares;
 };
