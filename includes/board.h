7a2c2ca4f3a09d7d5843933aa530b2aa9fb1a324
Thomas Byr <tbouyer2000@gmail.com>
2022-03-06 15:05:52 +0100

    new game board with more methods

---
+++ includes/board.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+#include "move.h"
+#include "piece.h"
+
+#include "bishop.h"
+#include "king.h"
+#include "knight.h"
+#include "pawn.h"
+#include "queen.h"
+#include "rook.h"
+
+static const std::string &fen_start =
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+class Board {
+  public:
+    Board();
+    ~Board();
+    void make_move(const Move &move);
+    void unmake_move(const Move &move);
+
+    void clear_board();
+    void load_from_fen(const std::string &fen);
+
+    int piece_at(int file, int rank) const;
+
+  private:
+    int board[64];
+    int map[64][6];
+
+    std::vector<Piece *> white_pieces;
+    std::vector<Piece *> black_pieces;
+
+    std::unordered_set<int> white_controlled_squares;
+    std::unordered_set<int> black_controlled_squares;
+};
